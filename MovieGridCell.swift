d4b3e8efee096fa0d67523b798d3fed8ba46f4bf
Tiffany <71067113+TiffanyLee016@users.noreply.github.com>
2020-11-02 18:23:32 -0800

    Adding Files Via Download

---
+++ MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixister
+//
+//  Created by Tiffany Lee on 11/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+   
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
